387c8fc8acb6dc5f3f4e45fc0eb1dacfa6431680
gavemit <ducminh21092004@gmail.com>
2024-09-30 19:58:49 +0700

    swt

---
+++ src/Displ/Display.java
@@ -0,0 +1,14 @@
+package Displ;
+
+import java.util.ArrayList;
+
+
+public class Display {
+    public void displayMenu(){
+        System.out.println("=====Task Program=====");
+        System.out.println("\t1. Add Task");
+        System.out.println("\t2. Delete Task");
+        System.out.println("\t3. Display Task");
+        System.out.println("\t4. exit");
+    }
+}
